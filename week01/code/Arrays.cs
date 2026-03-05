f6eaf48aae0bef2e7e1dcadeb4af22220ad3ccde
Ethan O'Brien <ethan.a.obrienw@gmail.com>
2026-03-04 20:42:35 -0600

    Week 1 code activity

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,12 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        var result = new double[length];
+        for (var i = 0; i < result.Length; i++) {
+            result[i] = (i + 1) * number;
+        }
 
-        return []; // replace this return statement with your own
+        return result; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +33,11 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        List<int> elementsToMove = data.GetRange(data.Count - amount, amount);
+
+        data.RemoveRange(data.Count - amount, amount);
+
+        data.InsertRange(0, elementsToMove);
     }
 }
